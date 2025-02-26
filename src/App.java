44cadf59225a98ed0976b8b4b22bccd938aa4961
Malaika Poluscar <polusmal001@warren.k12.in.us>
2025-02-26 13:55:34 -0500

    km

--- src/App.java
+++ src/App.java
@@ -1,2 +1,11 @@
-package PACKAGE_NAME;public class App {
+import java.util.List;
+import java.util.stream.Stream;
+
+public class App {
+    public static void main(String[] args) {
+        var evens = Stream.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+                .filter(t -> t % 2 != 0);
+    }
+
+
 }
