af0a3be778b0aadc3e88fb4ad7fa1c9667475efc
Malaika Poluscar <polusmal001@warren.k12.in.us>
2025-02-27 07:40:01 -0500

    km

--- src/App.java
+++ src/App.java
@@ -1,11 +1,17 @@
 import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class App {
     public static void main(String[] args) {
-        var evens = Stream.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
-                .filter(t -> t % 2 != 0);
+        var odds = Stream.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+                .filter(t -> t % 2 != 0)
+                .collect(Collectors.toList());
+
+        System.out.println(odds);
+
     }
+
 
 
 }
